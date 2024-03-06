ecf2abb70d946f342dd4526bbd92e79ba66531ca
Justin Lo <justinlctstudy96@gmail.com>
2024-03-06 22:05:56 +0000

    added files for docker image creation

--- drizzle/scripts/seed.ts
+++ drizzle/scripts/seed.ts
@@ -1,12 +1,12 @@
 import { NewAccount, insertAccount } from "@/drizzle/queries/auth-accounts.query";
 
 async function main() {
-    const newAccount: NewAccount = {
-        name: "Justin",
-        email: "justin@gmail.com",
-        password: "justin"
-    };
-    const res = await insertAccount(newAccount);
+    // const newAccount: NewAccount = {
+    //     name: "Justin",
+    //     email: "justin@gmail.com",
+    //     password: "justin"
+    // };
+    // const res = await insertAccount(newAccount);
     // console.log("insert account success", res);
     process.exit();
 }
