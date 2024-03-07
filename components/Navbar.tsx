9e20752df4e7c93ec2cfb571b34c0f0d8d9ab900
Justin Lo <justinlctstudy96@gmail.com>
2024-03-07 16:58:00 +0000

    cd9

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = async () => {
             <div className='container flex items-center justify-between'>
                 <Link href='/'>
                     <HandMetal />
-                    Test8
+                    Test9
                 </Link>
                 {session?.user ? (
                     <UserAccountNav />
