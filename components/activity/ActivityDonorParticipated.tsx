1716d6dd0ce4fc85563cc65253eeadb9cfca47d3
Justin Lo <justinlctstudy96@gmail.com>
2025-01-27 22:55:54 +0000

    added loading for ActivityDonorParticipated.tsx background image

--- components/activity/ActivityDonorParticipated.tsx
+++ components/activity/ActivityDonorParticipated.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import React, { useEffect, useState } from 'react';
 import { loadActivityThumbnailUrl } from "@/utils/loadBucket/loadBucketUrls";
 import { useRouter } from "next/navigation";
@@ -8,6 +7,7 @@ import { format } from 'date-fns';
 import { capitalizeFirstLetter, getBorderColor, getStatusColor, getStatusText } from '@/utils/helperFunc';
 import { Calendar, Clock, MapPin } from 'lucide-react';
 import { ATTENDANCE_STATUS_NAMES } from '@/utils/constants';
+import { Loading } from "@/components/ui/loading";
 
 interface ActivityDonorParticipatedProps {
     isDonorUser: boolean;
@@ -25,10 +25,12 @@ interface ActivityDonorParticipatedProps {
 const ActivityDonorParticipated: React.FC<ActivityDonorParticipatedProps> = ({ isDonorUser, activityId, name, startAt, endAt, location, address, background, activityStatus, attendanceRecord }) => {
     const router = useRouter();
     const [backgroundUrl, setThumbnailUrl] = useState<string>('');
+    const [loadingBackground, setLoadingBackground] = useState<boolean>(true);
 
     const fetchThumbnail = async () => {
         const url = await loadActivityThumbnailUrl(background ?? '');
         setThumbnailUrl(url);
+        setLoadingBackground(false);
     }
 
     const handleActivityClick = () => {
@@ -81,12 +83,12 @@ const ActivityDonorParticipated: React.FC<ActivityDonorParticipatedProps> = ({ i
         );
     }
 
-
     return (
-        <div className={`cursor-pointer activity m-4 rounded-lg bg-cover bg-center h-48 w-full max-w-screen-2xl`}
+        <div className={`cursor-pointer activity m-4 rounded-lg bg-cover bg-center h-48 w-full max-w-screen-2xl relative`}
             style={{ backgroundImage: `url('${backgroundUrl}')` }}
             onClick={handleActivityClick}
         >
+            {loadingBackground && <Loading hasHeight={false} hasText={false} absolute={true} />}
             <div className={`bg-black bg-opacity-60 p-4 text-sm h-full flex flex-col justify-between rounded-lg hover:bg-neutral-700/90 border-8 ${borderColor}`}>
                 <h3 className="text-2xl font-bold text-white">{name}</h3>
                 <div className="flex flex-row justify-between items-center">
--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -7,12 +7,13 @@ interface LoadingProps {
   hasText?: boolean;
   hasHeight?: boolean;
   justifyCenter?: boolean;
+  absolute?: boolean;
 }
 
 const Loading = React.forwardRef<HTMLInputElement, LoadingProps>(
-  ({ hasText = true, hasHeight = true, justifyCenter = true }, ref) => {
+  ({ hasText = true, hasHeight = true, justifyCenter = true, absolute = false }, ref) => {
     return (
-      <div className={cn('w-full h-full flex flex-col items-center justify-center', hasHeight && 'my-40')}>
+      <div className={cn('w-full h-full flex flex-col items-center justify-center', hasHeight && 'my-40', absolute && 'absolute inset-0')}>
         {hasText && <h3 className={cn('py-6 text-center font-bold')}>Loading</h3>}
         <div className={cn('flex items-center', justifyCenter && 'justify-center')}>
           <div className='loader'></div>
