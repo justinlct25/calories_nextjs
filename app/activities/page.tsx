619884dfdbb2104ce8e8af6f25b9f77be096c151
Justin Lo <justinlctstudy96@gmail.com>
2025-01-29 22:56:22 +0000

    updated navbar and activities setting active tab

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -5,6 +5,8 @@ import { PlusCircle } from "lucide-react";
 import ActivitiesSelectionPanel from "@/components/activity/ActivitiesSelectionPanel";
 import TopPadding from '@/components/TopPadding';
 import { useUserStore } from '../stores/user-store-provider';
+import { useEffect } from 'react';
+import { useActiveTabStore } from '../stores/active-tab-store';
 
 
 const ActivitiesMenuPage = () => {
@@ -12,15 +14,27 @@ const ActivitiesMenuPage = () => {
         (state) => state,
     )
 
+    const { setActiveTab } = useActiveTabStore((state) => ({
+        setActiveTab: state.setActiveTab,
+    }));
+
+    useEffect(() => {
+        setActiveTab('/activities');
+    }, [setActiveTab]);
+
 
     return (
         <div className='w-full '>
             <TopPadding />
-            <div className='container flex items-center justify-between max-w-screen-xl mx-auto'>
-            <h2 className="text-3xl font-bold px-20">Exercise Activities</h2>
-                {user.isAdmin && <Link href='/activities/create'><PlusCircle /></Link>}
+            <div className='container flex items-center justify-between max-w-screen-xl mx-auto relative'>
+                {/* <h2 className="text-3xl font-bold px-20">Exercise Activities</h2> */}
+                {user && user.isAdmin && (
+                    <Link href='/activities/create' className='absolute top-6 right-4'>
+                        <PlusCircle className='h-8 w-8 ' />
+                    </Link>
+                )}
             </div>    
-            <ActivitiesSelectionPanel isAdmin={user.isAdmin} />
+            <ActivitiesSelectionPanel isAdmin={user ? user.isAdmin : false} />
             {/* <div>
                 User: {JSON.stringify(user)}
             </div> */}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,25 +25,25 @@ const Navbar = () => {
 
 
     return (
-        // <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full top-0'>
         <div className='navbar py-2 border-s-zinc-200 fixed w-full top-0 z-50 h-14'>
             <div className='container flex items-center justify-between'>
-                <Link href='/' className="flex items-center space-x-2">
-                    <HandMetal />
-                    <span>Home</span>
+                <Link href='/' className={`flex items-center space-x-2 ${activeTab === '/' ? 'text-[#25ad91]' : ''}`} onClick={() => handleTabClick('/')}>
+                    <img src='/images/foodsport_icon.png' alt='Logo' className='h-8' />
+                    {/* <HandMetal /> */}
+                    {/* <span>Home</span> */}
                 </Link>
-                <Link href='/activities'>
+                <Link href='/activities' className={`${activeTab === '/activities' ? 'text-[#25ad91]' : ''}`} onClick={() => handleTabClick('/activities')}>
                     Exercise Activities
                 </Link>
-                <Link href='/'>
+                <Link href='/donations' className={`${activeTab === '/donations' ? 'text-[#25ad91]' : ''}`} onClick={() => handleTabClick('/donations')}>
                     Donations
                 </Link>
-                <Link href='/'>
+                <Link href='/about' className={`${activeTab === '/about' ? 'text-[#25ad91]' : ''}`} onClick={() => handleTabClick('/about')}>
                     About Us
                 </Link>
                 {user && Object.keys(user).length !== 0 ? (
                     <>
-                        <Link href={`/donors/${user.donor.id}`}>
+                        <Link href={`/donors/${user.donor.id}`} className={`${activeTab === `/donors/${user.donor.id}` ? 'text-[#25ad91]' : ''}`} onClick={() => handleTabClick(`/donors/${user.donor.id}`)}>
                             <CircleUserRound />
                         </Link>
                         <SignOutBtn />
