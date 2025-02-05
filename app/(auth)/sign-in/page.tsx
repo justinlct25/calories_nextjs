0d563756a148ff612a8fff61e7520c3a1f6894db
Justin Lo <justinlctstudy96@gmail.com>
2025-02-05 00:02:20 +0000

    updated the way to use the Suspense

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -5,21 +5,25 @@ import TopPadding from "@/components/TopPadding";
 import SignInForm from "@/components/forms/SignInForm";
 import { useSearchParams } from "next/navigation";
 
-export default function Page() {
+function SignInContent() {
     const params = useSearchParams();
     const activityId = params.get('activityId');
-    const numActivityId = activityId ? parseInt(activityId) : null
-    // console.log(JSON.stringify(activityParams))
+    const numActivityId = activityId ? parseInt(activityId) : null;
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className='w-full h-screen flex items-center justify-center'>
-                <TopPadding />
-                <div className='w-full'>
-                    <SignInForm activityId={numActivityId} />  
-                </div>
+        <div className='w-full h-screen flex items-center justify-center'>
+            <TopPadding />
+            <div className='w-full'>
+                <SignInForm activityId={numActivityId} />
             </div>
-        </Suspense>
-    )
-};
+        </div>
+    );
+}
 
+export default function Page() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <SignInContent />
+        </Suspense>
+    );
+}
